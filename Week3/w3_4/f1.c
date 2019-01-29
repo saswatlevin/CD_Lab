b4a51efcda4bd8672fe60b851e88e39f6a136390
Saswat Levin <saswatlevin@gmail.com>
2019-01-30 00:37:36 +0530

    added Week3

---
+++ Week3/w3_4/f1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int var=0;
+printf("\nHello World!");
+printf("\nEnter an integer");
+scanf("%d",&var);
+printf("\nThe integer entered is=%d\t",var);
+return 0;
+}
+
---
+++ Week3/w3_4/f2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int var=0;
+write("\nHello World!");
+write("\nEnter an integer");
+read("%d",&var);
+write("\nThe integer entered is=%d\t",var);
+return 0;
+}
+
